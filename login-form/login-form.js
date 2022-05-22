8b57720c309748b371d696b32b935a28e28aa7f2
Marina Chekmeneva <101263754+Ma1vina@users.noreply.github.com>
2022-05-23 00:43:06 +0300

    Add files via upload

--- login-form/login-form.js
+++ login-form/login-form.js
@@ -22,3 +22,21 @@ function getEntrance() {
     alert("Inccorect login or password!");
 }
 }
+
+
+
+// var button = document.getElementById("show");
+//          button.onclick = show;
+//          var input = document.getElementById("Pwd");
+//          var icon = document.getElementById("show1");
+//          function show () {
+//           if(input.getAttribute('type') == 'password') {
+//            input.removeAttribute('type');
+//            input.setAttribute('type', 'text');
+//            button.innerHTML='Hide password  <i id="Pwd" class="far fa-eye-slash">';
+//           } else {
+//            input.removeAttribute('type');
+//            input.setAttribute('type', 'password');
+//            button.innerHTML='Show password  <i id="swow1" class="checkPwd">';
+//           }
+//          }
