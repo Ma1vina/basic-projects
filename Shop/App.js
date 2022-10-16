59f779051c59a823cf2968c4cfbfb0547020e7db
Marina Chekmeneva <101263754+Ma1vina@users.noreply.github.com>
2022-10-16 18:49:21 +0300

    Add files via upload

--- Shop/App.js
+++ Shop/App.js
@@ -10,6 +10,7 @@ import './App.css';
 import { Elem } from './Elem';
 import { Header } from './Header';
 
+
 function App() {
 const[count1, setCount1] = useState(0);
 const[count2, setCount2] = useState(0);
@@ -19,6 +20,23 @@ const[count5, setCount5] = useState(0);
 const[count6, setCount6] = useState(0);
 
 const[countAll, setCountAll] = useState(0);
+
+let all = [count1,count2,count3,count4,count5,count6];
+
+
+// let summm = 0;
+//  function red(arr){
+// for(let i =0; i<arr.length;i++){
+//   summm = summm + arr[i]
+// }
+// return summm 
+//  } 
+// red(all);
+
+let basketC = all.reduce((acc,v)=>{
+  return acc+v
+},0)
+
 
 
 function hundlerIncrease(state,funcState,sum){
@@ -40,16 +58,17 @@ function hundlerDecrease(state,funcState, sum){
   return (
 
     <div className="App">
-       <Header    stateAll={countAll} funcAll={setCountAll} 
+       <Header basketC={basketC}
        classNameBasket={'basket'} className={"containerHeader"} classNameLog={'logoStyle'} src={logo} alt={"logo"} width={"70px"} height={"70px"}/>
 
      <div className='containerBody'>
-      <Elem src={logo1} alt={"logo1"} width={"250px"} height={"250px"} description={"Бинокль Yukon Point 8*42"} increase={hundlerIncrease} counter={count1} funcSt={setCount1} decrease ={hundlerDecrease} sale={"12 150 ₽"}/>
-       <Elem  src={logo2} alt={"logo2"} width={"250px"} height={"250px"} description={"Туристический компас SUUNTO MB-6 Global"} increase={hundlerIncrease} counter={count2} funcSt={setCount2} decrease ={hundlerDecrease} sale={"5 190 ₽"}/>
-       <Elem  src={logo3} alt={"logo3"} width={"250px"} height={"250px"} description={"Фонарь PO1 для кемпинга и туризма с солнченой батареей"} increase={hundlerIncrease} counter={count3} funcSt={setCount3} decrease ={hundlerDecrease} sale={"2 100 ₽"}/>
-       <Elem  src={logo4} alt={"logo4"} width={"250px"} height={"250px"} description={"Жаровня BOYSCOUT"} increase={hundlerIncrease} counter={count4} funcSt={setCount4} decrease ={hundlerDecrease} sale={"10 199 ₽"}/>
-       <Elem  src={logo5} alt={"logo5"} width={"250px"} height={"250px"} description={"Изотермическая сумка Aceline VB-20"} increase={hundlerIncrease} counter={count5} funcSt={setCount5} decrease ={hundlerDecrease} sale={"2 899 ₽"}/>
-       <Elem  src={logo6} alt={"logo6"} width={"250px"} height={"250px"} description={"ТФонарь FinePower HM-100"} increase={hundlerIncrease} counter={count6} funcSt={setCount6} decrease ={hundlerDecrease} sale={"1 099 ₽"}/>
+
+      <Elem src={logo1} alt={"logo1"} width={"150px"} height={"150px"} description={"Бинокль Yukon Point Greet-Е 8*42"} increase={hundlerIncrease} counter={count1} funcSt={setCount1} decrease ={hundlerDecrease} sale={"12 150 ₽"}/>
+       <Elem  src={logo2} alt={"logo2"} width={"150px"} height={"150px"} description={"Туристический компас SUUNTO MB-6 Global"} increase={hundlerIncrease} counter={count2} funcSt={setCount2} decrease ={hundlerDecrease} sale={"5 190 ₽"}/>
+       <Elem  src={logo3} alt={"logo3"} width={"150px"} height={"150px"} description={"Фонарь для кемпинга и туризма на солнечной батарее"} increase={hundlerIncrease} counter={count3} funcSt={setCount3} decrease ={hundlerDecrease} sale={"2 100 ₽"}/>
+       <Elem  src={logo4} alt={"logo4"} width={"150px"} height={"150px"} description={"Многофункциональная жаровня BOYSCOUT-NM89"} increase={hundlerIncrease} counter={count4} funcSt={setCount4} decrease ={hundlerDecrease} sale={"10 199 ₽"}/>
+       <Elem  src={logo5} alt={"logo5"} width={"150px"} height={"150px"} description={"Изотермическая сумка Aceline VB-20 для туризма"} increase={hundlerIncrease} counter={count5} funcSt={setCount5} decrease ={hundlerDecrease} sale={"2 899 ₽"}/>
+       <Elem  src={logo6} alt={"logo6"} width={"150px"} height={"150px"} description={"Фонарь FinePower HM-100"} increase={hundlerIncrease} counter={count6} funcSt={setCount6} decrease ={hundlerDecrease} sale={"1 099 ₽"}/>
      </div>
     </div>
   );
--- Shop/Header.js
+++ Shop/Header.js
@@ -7,7 +7,7 @@ import React from "react";
     <div className={props.className}>
         <img className={props.classNameLog} src={props.src} alt={props.alt} width={props.width} height={props.height}></img>
     <span><h1>Shop</h1>The market for recreation, leisure and tourism products</span>
-    <div className={props.classNameBasket}>Корзинa ({props.stateAll})
+<div className={props.classNameBasket}>Корзинa({props.basketC})
     </div>
     </div>
  )
