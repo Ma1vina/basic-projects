be4948ba3c5a92818944b2b32f89a00e6574cc3b
Marina Chekmeneva <101263754+Ma1vina@users.noreply.github.com>
2022-10-24 22:45:54 +0300

    Add files via upload

---
+++ Shop/new/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+ export function Header(props){
+
+
+ return(
+    <div className={props.className}>
+        <img className={props.classNameLog} src={props.src} alt={props.alt} width={props.width} height={props.height}></img>
+    <span><h1>Shop</h1>The market for recreation, leisure and tourism products</span>
+    <div className={props.classNameBasket}>Корзинa ({props.value})
+    </div>
+    </div>
+ )
+ }
