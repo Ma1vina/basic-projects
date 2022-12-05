4ecea396e0ccdffdcdab9025ef913a2b2f09bf52
Marina Chekmeneva <101263754+Ma1vina@users.noreply.github.com>
2022-12-05 22:05:19 +0300

    Add files via upload

--- redux-posts-editor/App.js
+++ redux-posts-editor/App.js
@@ -1,26 +1,19 @@
 import { useState } from "react";
 import "./App.css";
+import { Counter } from "./components/Counter";
 
 
 function App() {
-  const [likes, setLikes] = useState(0);
   const [value, setValue] = useState("тут будет текст")
 
-  function increment(){
-   return setLikes(likes +1)
-  }
-
-  function decrement(){
-    return setLikes(likes -1)
-   }
+  
 
   return (
     <div>
-      <div>{likes}</div>
-      <div>{value}</div>
-      <input type="text" value={value} onChange = {event => setValue(event.target.value)}></input>
-      <button onClick={increment}>increment</button>
-      <button onClick={decrement}>decrement</button>
+<Counter/>
+      {/* <div>{value}</div>
+      <input type="text" value={value} onChange = {event => setValue(event.target.value)}></input> */}
+      
     </div>
   );
 }
