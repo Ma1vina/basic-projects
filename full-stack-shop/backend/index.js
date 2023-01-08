2e74a6a9df77ec93ea227bb4ce5d26b195e30340
Marina Chekmeneva <101263754+Ma1vina@users.noreply.github.com>
2023-01-09 00:50:20 +0300

    Add files via upload

--- full-stack-shop/backend/index.js
+++ full-stack-shop/backend/index.js
@@ -16,8 +16,42 @@ app.listen(PORT, () => {
   console.log(`сервер работает на ${PORT}`);
 });
 
-app.get("/", (req, res) => {
-  res.status(200).json(data);
-});
+// app.get("/", (req, res) => {
+// res.status(200).json(data);
+// });
 
+// let res = [];
+// let value1=2000;
+// let value2=3000;
+// data.map((elem) =>{
+//   if(elem.price >= value1 && elem.price <= value2){
+// res.push(elem)
+// }})
+// console.log(res);
+
+app.get("/prod", (req, res) => {
+  let elem = req.query.elem; //cколько вывести элементов
+  let page = req.query.page; //страничка
+  let min = req.query.min;
+  let max = req.query.max;
+  console.log(min, max);
+
+  elem = Number(elem);
+  page = Number(page);
+  min = Number(min);
+  max = Number(max);
+
+  let filterProds = [];
+  data.map((el) => {
+    if (el.price >= min && el.price <= max) {
+      filterProds.push(el);
+    }
+  });
+  const result = [];
+  for (let i = 0; i < filterProds.length; i = i + elem) {
+    const pageArr = filterProds.slice(i, i + elem);
+    result.push(pageArr);
+  }
+  res.status(200).json({ products: result[page], sizePages: result.length });
+});
 app.use("/static", expressstatic(__dirname + "/public"));
